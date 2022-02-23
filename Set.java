cf61ebf9bf1c09b8038560f4a472600c0939a9fa
spenechap444 <86894473+spenechap444@users.noreply.github.com>
2022-02-23 13:18:46 -0500

    Add files via upload

---
+++ Set.java
@@ -0,0 +1,10 @@
+public class Set {
+    boolean warmup;
+    double weight;
+    double reps;
+    Set(double w, double r, boolean warmup) {
+        this.weight = w;
+        this.reps = r;
+        this.warmup = warmup;
+    }
+}
